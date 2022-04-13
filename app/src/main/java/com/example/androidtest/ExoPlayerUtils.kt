ebe3169a9feab67ecb0f811599bc99b5ae935864
sss193 <sunny4get@gmail.com>
2022-04-14 01:20:01 +0530

    feature 3 integrated

--- app/src/main/java/com/example/androidtest/ExoPlayerUtils.kt
+++ app/src/main/java/com/example/androidtest/ExoPlayerUtils.kt
@@ -1,6 +1,10 @@
 package com.example.androidtest
 
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
@@ -8,8 +12,16 @@ import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import java.util.*
 
 class ExoPlayerUtils(private val context: Context, private val playerView: PlayerView) {
+
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
+
     private var mPlayer: SimpleExoPlayer? = null
     private val videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
 
@@ -30,6 +42,9 @@ class ExoPlayerUtils(private val context: Context, private val playerView: Playe
         // Prepare the player.
         mPlayer!!.prepare()
 
+         //initialize shake detection
+         initSensorManager()
+
     }
 
     //creating mediaSource
@@ -44,11 +59,53 @@ class ExoPlayerUtils(private val context: Context, private val playerView: Playe
     }
 
      fun releasePlayer() {
+         sensorManager!!.unregisterListener(sensorListener)
         if (mPlayer == null) {
             return
         }
         //release player when done
         mPlayer!!.release()
         mPlayer = null
+    }
+
+
+    private fun initSensorManager(){
+        // Getting the Sensor Manager instance
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+
+            // Fetching x,y,z values
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            // Getting current accelerations
+            // with the help of fetched x,y,z values
+            currentAcceleration = Math.sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            // acceleration value is over 12
+            if (acceleration > 12) {
+                mPlayer?.pause()
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
     }
 }
